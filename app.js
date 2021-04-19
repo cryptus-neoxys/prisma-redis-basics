aa32887a9a71e3f5cd2fc807b27f5f3e936f2bb1
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-20 02:46:24 +0530

    ✨ Create Users

--- app.js
+++ app.js
@@ -1,15 +1,69 @@
 const { PrismaClient } = require("@prisma/client");
+const express = require("express");
+const { body, validationResult } = require("express-validator");
 
 const prisma = new PrismaClient();
 
-async function main() {
-  try {
-    const users = await prisma.users.findMany();
+const PORT = process.env.PORT || 5000;
 
-    console.log(users);
+const app = express();
+app.use(express.json());
+
+const userValidationRules = [
+  body("email")
+    .isLength({ min: 1 })
+    .withMessage("email can't be empty")
+    .isEmail()
+    .withMessage("Must be a valid email"),
+  body("name").isLength({ min: 1 }).withMessage("name can't be empty"),
+  body("role")
+    .isIn(["USER", "ADMIN", "SUPERUSER", undefined])
+    .withMessage(
+      `Invalid Role, must be one of ['USER', 'ADMIN', 'SUPERUSER', undefined]`
+    ),
+];
+
+const simpleValidationResults = validationResult.withDefaults({
+  formatter: (err) => err.msg,
+});
+
+const checkUser = (req, res, next) => {
+  const errors = simpleValidationResults(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.mapped());
+  }
+
+  next();
+};
+
+// Create
+app.post("/users", userValidationRules, checkUser, async (req, res) => {
+  const { name, email, role } = req.body;
+
+  try {
+    const existingUser = await prisma.user.findFirst({ where: { email } });
+    if (existingUser) {
+      throw { email: "email already exists" };
+    }
+    const user = await prisma.user.create({
+      data: { name, email, role },
+    });
+
+    return res.json(user);
   } catch (error) {
     console.error(error);
+    return res.status(400).json({ success: false, error });
   }
-}
+});
 
-main();
+// Read
+// Update
+// Delete
+// Find
+
+// Create Post
+// Read all Posts
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost${PORT}`);
+});
