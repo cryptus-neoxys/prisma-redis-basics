9a612e35747e726adf5048c6059932175787b0cf
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-25 16:13:32 +0530

    ♻️ connect.ts

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,10 +1,6 @@
-import express from "express";
-import responseTime from "response-time";
 import { PrismaClient } from "@prisma/client";
 import redis from "redis";
 import { promisify } from "util";
-import rateLimit from "express-rate-limit";
-import RedisStore from "rate-limit-redis";
 
 export const client = redis.createClient();
 export const getAsync = promisify(client.get).bind(client);
