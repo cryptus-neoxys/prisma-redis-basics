19dd73ba65a7ea163b27d5801de195c3bbb4b23e
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-05-01 07:31:07 +0530

    ♻️ dbConnect

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,9 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import redis from "redis";
-import { promisify } from "util";
-
-export const client = redis.createClient();
-export const getAsync = promisify(client.get).bind(client);
-export const setAsync = promisify(client.set).bind(client);
 
 export const prisma = new PrismaClient();
